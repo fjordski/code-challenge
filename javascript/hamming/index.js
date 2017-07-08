c27ebb490467d40c5f330128880b07476bde17fc
fjordski <fordheacock@gmail.com>
2017-07-08 15:21:36 -0700

    Update index.js

--- javascript/hamming/index.js
+++ javascript/hamming/index.js
@@ -2,6 +2,18 @@
 // `npm test` to ensure the unit tests pass and
 // your code meets all of the conditions.
 // You may use ES6 or ES5 to solve.
+const Hamming = function() {};
+Hamming.prototype.compute = function(strand1, strand2) {
+    let distance = 0;
+    if (strand1.length == strand2.length) {
+        for (i = 0; i < strand1.length; i++) {
+            if (strand1[i] !== strand2[i]) distance++;
+        }
+    } else {
+        throw new Error("DNA strands must be of equal length.");
+    }
+    return distance;
+};
 
 
 module.exports = Hamming;
